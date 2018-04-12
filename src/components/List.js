a5b9c54b94b6ee52b8d11d896fb3cb6f96b581fe
Vram Kupelian <vramkupelian@gmail.com>
2018-04-11 19:31:18 -0700

    Update 1.0.0.1 - Fixed image paths (relative to public folder, not App.js)

--- src/components/List.js
+++ src/components/List.js
@@ -5,7 +5,7 @@ const List = props => (
   <div className="list-group">
     {props.characters.map(item=>{ 
       return (
-      <div key = {item.id}>{item.id}. {item.name} <img key = {item.id} src = {item.image} alt = "character" /></div>
+      <div key = {item.id}> <img key = {item.id} src = {item.image} alt = "character" /></div>
       );    
    })}
   </div>
