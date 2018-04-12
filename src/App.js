8aea9894fabf2b61fcbe3b79b909570300153558
Vram Kupelian <vramkupelian@gmail.com>
2018-04-11 20:18:43 -0700

    Update 1.0.3 - Removed randomizer, added shuffle for array. Working

--- src/App.js
+++ src/App.js
@@ -76,11 +76,14 @@ const characters = [
   },   
 ];
 
-let theCharacter;
-const randomizer = () => {
-theCharacter = characters[Math.floor(Math.random()*characters.length)]
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
-
+shuffle(characters);
 const App = () => <List characters={characters} />;
 
 export default App;
